08c24c00fe693a5ac47906aa3c80deb30db7cde3
DianaApz <kari_dia_12@hotmail.com>
2018-05-30 10:45:45 -0500

    actualizando css y html

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 //mandamos llamar a los elementos
-let btn = document.getElementById('btn');
-let cipher = document.getElementById('cipher');
-let decipher = document.getElementById('decipher');
+let introduction= document.getElementById('introduction');
+let cipher= document.getElementById('cipher');
+let decipher= document.getElementById('decipher');
 
-cipher.style.visibility = 'hidden';
-decipher.style.visibility = 'hidden';
+cipher.style.visibility= 'hidden';
+decipher.style.visibility= 'hidden';
+introduction.style.visibility='visible'; 
 
+//inicio
+let home= document.getElementById('home');
+home.addEventListener('click',inicio);
+
+function inicio(){
+    cipher.style.visibility= 'hidden';
+    decipher.style.visibility= 'hidden';
+    introduction.style.visibility='visible'; 
+}
 
 //cifrar
-let btn1 = document.getElementById('btn1');
-btn1.addEventListener('click', cifrar)
+let pageOne= document.getElementById('pageOne');
+pageOne.addEventListener('click', cifrar);
+
+
 
 function cifrar() {
-  btn.style.visibility='hidden';
+  introduction.style.visibility='hidden';
+  decipher.style.visibility = 'hidden';
   cipher.style.visibility= 'visible';
+
   let send1=document.getElementById('send1');
   send1.addEventListener('click', code);
 }
 
 function code() {
-    let key1= parseInt(document.getElementById('key1').value);
+    let number1= parseInt(document.getElementById('number1').value);
     let phrase1 = document.getElementById('phrase1').value;
     let answerCipher = '';
  
     for (let i = 0; i < phrase1.length; i++) {
         let positionAscii = phrase1.charCodeAt(i);
         if(65<= positionAscii && positionAscii<=90){
-            answerCipher += String.fromCharCode((positionAscii-65+key1)%26+65); 
+            answerCipher += String.fromCharCode((positionAscii-65+number1)%26+65); 
         }
         else if(97<= positionAscii && positionAscii<=122 ){
-            answerCipher += String.fromCharCode((positionAscii-97+key1)%26+97);
+            answerCipher += String.fromCharCode((positionAscii-97+number1)%26+97);
             
         }
         else {
@@ -37,36 +51,37 @@ function code() {
 
         }
     }
-    document.getElementById('cifrado').innerHTML=answerCipher;
+    document.getElementById('answer1').innerHTML=answerCipher;
     
 
 }
 
 //decifrar
-let btn2 = document.getElementById('btn2');
-btn2.addEventListener('click', decifrar);
+let pageTwo = document.getElementById('pageTwo');
+pageTwo.addEventListener('click', decifrar);
 
 function decifrar(){
-    btn.style.display = 'hidden';
-    decipher.style.visibility ='visible';
+    introduction.style.visibility = 'hidden';
     cipher.style.display = 'none';
+    decipher.style.visibility ='visible';
+    
     let send2=document.getElementById('send2');
     send2.addEventListener('click', decode);
 }
    
 function decode() {
        
-    let key2= parseInt(document.getElementById('key2').value);
+    let number2= parseInt(document.getElementById('number2').value);
     let phrase2 = document.getElementById('phrase2').value;
     let answerDecipher = '';
     
     for (let i = 0; i < phrase2.length; i++) {
         let positionAsciiD = phrase2.charCodeAt(i);
         if(65<= positionAsciiD && positionAsciiD<=90){
-         answerDecipher += String.fromCharCode((positionAsciiD-90-key2)%26+90); 
+         answerDecipher += String.fromCharCode((positionAsciiD-90-number2)%26+90); 
         }
         else if(97<= positionAsciiD && positionAsciiD<=122 ){
-         answerDecipher += String.fromCharCode((positionAsciiD-122-key2)%26+122);
+         answerDecipher += String.fromCharCode((positionAsciiD-122-number2)%26+122);
                
         }
         else {
@@ -74,17 +89,8 @@ function decode() {
    
         }
     }
-   document.getElementById('descifrado').innerHTML=answerDecipher;
+    document.getElementById('answer2').innerHTML= answerDecipher;
     
 
 
-}
-
-let home = document.getElementById('home')
-home.addEventListener('click',inicio);
-
-function inicio(){
-    cipher.style.visibility = 'hidden';
-    decipher.style.visibility = 'hidden';
-    btn.style.visibility='visible'; 
 }
